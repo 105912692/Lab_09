bfae7fa9b0c162f8458e8b7a63fac479e594fc6e
Oliver Scott <105912692@student.swin.edu.au>
2025-05-07 15:19:32 +1000

    finished

---
+++ search_form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Car Search</title>
+
+  <h1>Car Search </h1>
+</head>
+<body>
+  <form method="GET" action="search_result.php">
+    <label>Search Car Model:</label>
+    <input type="text" name="model" required>
+    <input type="submit" value="Search">
+  </form>
+</body>
+</html>
--- settings.php
+++ settings.php
@@ -3,4 +3,9 @@
     $user = "root";          // default username for XAMPP's MySQL
     $pwd = "";              // default password is empty in XAMPP
     $sql_db = "exhibition_db";  // replace with the actual name of your database
+    $conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
