9cc08412bc486db7e8be0363eeffcdd80b76aba4
Oliver Scott <105912692@student.swin.edu.au>
2025-05-07 15:01:02 +1000

    sad

--- cars.php
+++ cars.php
@@ -3,19 +3,16 @@
     $dbconn = @mysqli_connect($host,$user,$pwd,$sql_db);
     if($dbconn) {
         $query = "SELECT * FROM cars";
-        $result = mysqli_query ($dbconn, $query)
-        if ($result) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td>" . $row['car_id'] . "</td>";
-                echo "<td>" . $row['make'] . "</td>";
-                echo "<td>" . $row['model'] . "</td>";
-                echo "<td>" . $row['price'] . "</td>";
-                echo "<td>" . $row['yom'] . "</td>";
-                echo "</tr>";
-            }
-        }          
-        else { echo "<p>There are no cars to display.</p>"; }
+        $result = mysqli_query ($dbconn, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>".$row['car_id']."</td>";
+            echo "<td>" . $row['make'] . "</td>";
+            echo "<td>" . $row['model'] . "</td>";
+            echo "<td>" . $row['price'] . "</td>";
+            echo "<td>" . $row['yom'] . "</td>";
+            echo "</tr>";
+        }        
         mysqli_close($dbconn);
     } else echo "<p>Unable to connect to db.</p>";
 ?>
